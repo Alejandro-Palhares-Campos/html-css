45f5d6d2c01daf49a3ea32890811c0264f3d68c0
Alejandro <alejandropalharescampos@gmail.com>
2022-05-03 09:57:05 -0300

    upd

---
+++ exercicio/acoes.js
@@ -0,0 +1,48 @@
+
+// Escopo global
+let s = document.querySelector('section#saida')
+
+function a1() {
+    s.innerHTML += '<p>Você clicou no <strong>um</strong> botão!</p>'
+}
+
+function a2() {
+    s.innerHTML += '<p>Você clicou no <strong>dois</strong> botão!</p>'
+}
+
+function a3() {
+    s.innerHTML += '<p>Você clicou no <strong>tres</strong> botão!</p>'
+}
+
+function a4() {
+    s.innerHTML += '<p>Você clicou no <strong>quarto</strong> botão!</p>'
+}
+
+function a5() {
+    s.innerHTML += '<p>Você clicou no <strong>cinco</strong> botão!</p>'
+}
+
+
+function a6() {
+    s.innerHTML += '<p>Você clicou no <strong>seis</strong> botão!</p>'
+}
+
+
+function a7() {
+    s.innerHTML += '<p>Você clicou no <strong>sete</strong> botão!</p>'
+}
+
+
+function a8() {
+    s.innerHTML += '<p>Você clicou no <strong>oito</strong> botão!</p>'
+}
+
+
+function a9() {
+    s.innerHTML += '<p>Você clicou no <strong>nove</strong> botão!</p>'
+}
+
+
+function a10() {
+    s.innerHTML += '<p>Você clicou no <strong>dez</strong> botão!</p>'
+}
